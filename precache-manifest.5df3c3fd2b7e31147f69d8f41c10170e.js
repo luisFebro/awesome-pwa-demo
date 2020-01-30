4181ee3712e346f6f5bbea78378f2d73520566d2
Luis Febro <mr.febro@gmail.com>
2020-01-29 22:51:22 -0400

    Updates

---
+++ precache-manifest.5df3c3fd2b7e31147f69d8f41c10170e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0f1235b5fb46800526d93dfc57467c",
+    "url": "/awesome-pwa-demo/index.html"
+  },
+  {
+    "revision": "350536caa2d309be6619",
+    "url": "/awesome-pwa-demo/static/css/main.4d2fa40a.chunk.css"
+  },
+  {
+    "revision": "a3a8deddb6d84fdc4e45",
+    "url": "/awesome-pwa-demo/static/js/2.f493e6cf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/awesome-pwa-demo/static/js/2.f493e6cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "350536caa2d309be6619",
+    "url": "/awesome-pwa-demo/static/js/main.9b103e17.chunk.js"
+  },
+  {
+    "revision": "684e8f7c97dd82871c07",
+    "url": "/awesome-pwa-demo/static/js/runtime-main.3aafcee6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/awesome-pwa-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
